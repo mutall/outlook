8ad365edac9db8411ed6b29bcc018bb8e2e5a88f
Peter Kamau <kamaupeter343@gmail.com>
2022-04-05 09:20:41 +0300

    ssh2 library testing

--- rentize/v/mpesa/mpesa.php
+++ rentize/v/mpesa/mpesa.php
@@ -1,25 +1,7 @@
 <?php
-//
-//Get the file to process.
-$file= file_get_contents("mpesa_texts.json");
-//
-//Convert the file into an array
-$values= json_decode($file, TRUE);
-//
-forEach($values as $value){
-    //
-    //Retrieve the account number
-    $account=preg_match('/(?<=Account:\s)\w+/', $value);
-    //
-    //Retrieve the name of the account holder
-    $name= preg_match('/(?<=Name:\s)\w+\s\w+\s\w+/',$value);
-    //Get the amount due
-    $amount= preg_match('/(?<=Amount Due:\s)\w+\S\w+\s\w+\s\w+/', $value);
-    //
-    //Get the due date
-    $due_date= preg_match('(?<=Due Date: ).*', $value);
-    //
-}
-?>
 
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
 
